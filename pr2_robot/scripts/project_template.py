489ad70616f5b9fb3c123237e27050dd2668cc9c
adammalpass <adammalpass@hotmail.co.uk>
2017-08-05 08:05:12 +0900

    feat: Add clustering. Basic functionality works, but needs optimising

--- pr2_robot/scripts/project_template.py
+++ pr2_robot/scripts/project_template.py
@@ -113,19 +113,50 @@ def pcl_callback(pcl_msg):
     # Extract outliers
     cloud_objects = cloud_passthrough.extract(inliers, negative=True)
 
-    # TODO: Euclidean Clustering
+    ######################## TODO: Euclidean Clustering ############################
+    white_cloud = XYZRGB_to_XYZ(cloud_objects)
+    tree = white_cloud.make_kdtree()
+
+    # Create a cluster extraction object
+    ec = white_cloud.make_EuclideanClusterExtraction()
+    # Set tolerances for distance threshold 
+    # as well as minimum and maximum cluster size (in points)
+    # NOTE: These are poor choices of clustering parameters
+    # Your task is to experiment and find values that work for segmenting objects.
+    ec.set_ClusterTolerance(0.015)
+    ec.set_MinClusterSize(20)
+    ec.set_MaxClusterSize(1500)
+    # Search the k-d tree for clusters
+    ec.set_SearchMethod(tree)
+    # Extract indices for each of the discovered clusters
+    cluster_indices = ec.Extract()
 
     # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
+    #Assign a color corresponding to each segmented object in scene
+    cluster_color = get_color_list(len(cluster_indices))
+
+    color_cluster_point_list = []
+
+    for j, indices in enumerate(cluster_indices):
+        for i, indice in enumerate(indices):
+            color_cluster_point_list.append([white_cloud[indice][0],
+                                            white_cloud[indice][1],
+                                            white_cloud[indice][2],
+                                             rgb_to_float(cluster_color[j])])
+
+    #Create new cloud containing all clusters, each with unique color
+    cluster_cloud = pcl.PointCloud_PointXYZRGB()
+    cluster_cloud.from_list(color_cluster_point_list)
 
     # TODO: Convert PCL data to ROS messages
     ros_cloud_objects = pcl_to_ros(cloud_objects)
     ros_cloud_table = pcl_to_ros(cloud_table)
-    #ros_cluster_cloud = pcl_to_ros(cluster_cloud)
+    ros_cluster_cloud = pcl_to_ros(cluster_cloud)
 
     # TODO: Publish ROS messages
     pcl_objects_pub.publish(ros_cloud_objects)
     pcl_table_pub.publish(ros_cloud_table)
-    #pcl_cluster_pub.publish(ros_cluster_cloud)
+    pcl_cluster_pub.publish(ros_cluster_cloud)
 
 # Exercise-3 TODOs:
 
@@ -202,7 +233,7 @@ if __name__ == '__main__':
     #pcl_test_pub = rospy.Publisher("/test_cloud", PointCloud2, queue_size=1)
     pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue_size = 1)
     pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue_size = 1)
-    #pcl_cluster_pub = rospy.Publisher("/pcl_cluster", PointCloud2, queue_size = 1)
+    pcl_cluster_pub = rospy.Publisher("/pcl_cluster", PointCloud2, queue_size = 1)
 
     # TODO: Load Model From disk
     #model = pickle.load(open('model_75.sav', 'rb'))
